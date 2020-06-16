4ed2d47da80f6128e072a5a4061a6ce6f61ae514
pbjer <prenticebjerk@gmail.com>
2020-06-16 11:18:03 -0700

    setup initial noteSlice initialState, reducer, thunks, selectors

---
+++ src/components/notes/noteSlice.js
@@ -0,0 +1,153 @@
+import { createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const noteSlice = createSlice({
+  name: 'notes',
+  initialState: {
+    user: {
+      loggedIn: false,
+      userId: null,
+    },
+    currentNote: {
+      id: null,
+      title: '',
+      body: '',
+      created_at: null,
+      updated_at: null
+    },
+    allNotes: []
+  },
+  reducers: {
+    // Redux Toolkit uses Immer under the hood to provide
+    // a 'mutation' like interface for updating state
+    setUser: (state, action) => {
+      state.user = action.payload;
+    },
+    setUserLoggedInStatus: (state, action) => {
+      state.user.loggedIn = action.payload;
+    },
+    setCurrentNote: (state, action) => {
+      state.currentNote = action.payload;
+    },
+    setCurrentTitle: (state, action) => {
+      state.currentNote.title = action.payload;
+    },
+    setCurrentBody: (state, action) => {
+      state.currentNote.body = action.payload;
+    },
+    setAllNotes: (state, action) => {
+      state.allNotes = action.payload;
+    },
+  },
+});
+
+export const {
+  setUser,
+  setCurrentNote,
+  setCurrentTitle,
+  setCurrentBody,
+  setAllNotes
+} = noteSlice.actions;
+
+// Thunks
+// TODO: refactor thunks use axios config and reduce duplicate code
+export const requestCreateNote = (payload) => async(dispatch) => {
+  let response = null;
+  const { userId, title, body, history } = payload;
+  const requestBody = { title, body };
+  try {
+    response = await axios.post(
+      `http://localhost:3030/user/${userId}/note`,
+      requestBody,
+      { withCredentials: true }
+    );
+    await dispatch(setCurrentNote(response.data))
+  } catch(e) {
+    console.log(e)
+  } finally {
+    if (response) {
+      const { id } = response.data
+      history.push(`/note/${id}`);
+    }
+  }
+}
+
+export const requestGetNotes = (userId) => async(dispatch) => {
+  let response = null;
+  try {
+    response = await axios.get(`http://localhost:3030/user/${userId}`, { withCredentials: true });
+  } catch(e) {
+    console.log(e)
+  } finally {
+    if (response) {
+      await dispatch(setAllNotes(response.data))
+    }
+  }
+}
+
+export const requestGetNoteById = (payload) => async(dispatch) => {
+  let response = null;
+  const { userId, id } = payload;
+  try {
+    response = await axios.get(`http://localhost:3030/user/${userId}/note/${id}`, { withCredentials: true });
+    await dispatch(await setCurrentNote(response.data))
+  } catch(e) {
+    console.log(e)
+  } finally {
+    if (response) {
+    }
+  }
+}
+
+export const requestUpdateNote = (payload) => async(dispatch) => {
+  const { userId, id, title, body } = payload;
+  const requestBody = { title, body }
+  let response = null;
+  try {
+    response = await axios.put(
+      `http://localhost:3030/user/${userId}/note/${id}`,
+      requestBody,
+      { withCredentials: true }
+    );
+    await dispatch(setCurrentNote(response.data))
+  } catch(e) {
+    console.log(e)
+  } finally {
+    if (response) {
+    }
+  }
+}
+
+export const requestDeleteNote = (payload) => async(dispatch) => {
+  let response = null;
+  const { userId, id, history } = payload;
+  try {
+    response = await axios.delete(`http://localhost:3030/user/${userId}/note/${id}`, { withCredentials: true });
+  } catch(e) {
+    console.log(e)
+  } finally {
+    if (response) {
+      await dispatch(clearCurrentNote());
+      history.push('/notes');
+    }
+  }
+}
+
+export const clearCurrentNote = () => (dispatch) => {
+  dispatch(setCurrentNote({
+    id: null,
+    title: '',
+    body: '',
+    created_at: null,
+    deleted_at: null,
+  }))
+}
+
+// Selectors
+export const selectUser = state => state.notes.user;
+export const selectAllNotes = state => state.notes.allNotes;
+export const selectCurrentNote = state => state.notes.currentNote;
+export const selectCurrentTitle = state => state.notes.currentNote.title;
+export const selectCurrentBody = state => state.notes.currentNote.title;
+
+export default noteSlice.reducer;
