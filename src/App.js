e3e1d2a45ddf807dbb0b2c4c85da99b2250e1339
pbjer <prenticebjerk@gmail.com>
2020-06-16 14:55:55 -0700

    read and set token/logged in status at root component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
 } from 'react-router-dom';
+import Cookies from 'js-cookie';
+import jwtDecode from 'jwt-decode';
+import { setUser } from './components/notes/noteSlice';
 import { Header } from './components/Header';
 import { Home } from './components/Home';
-import { AuthCallback } from './components/AuthCallback';
 import { NoteList } from './components/notes/NoteList';
 import { NoteForm } from './components/notes/NoteForm';
 import './styles/styles.scss';
 import './App.scss';
 
 export const App = () => {
+  const dispatch = useDispatch();
+  const token = Cookies.get('token');
+  if (token !== undefined) {
+    const { id } = jwtDecode(token);
+    dispatch(setUser({ userId:id, loggedIn: true }));
+  }
+
   return (
     <div id="app-wrapper">
       <Router>
@@ -22,9 +32,6 @@ export const App = () => {
             exact
             path="/"
             component={Home}/>
-          <Route
-            path="/auth-callback"
-            component={AuthCallback} />
           <Route
             path="/notes"
             component={NoteList} />
