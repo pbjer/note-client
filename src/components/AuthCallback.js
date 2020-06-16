7cf93961f9671ba1e2adebdfea10a745768dc544
pbjer <prenticebjerk@gmail.com>
2020-06-16 11:12:41 -0700

    set up auth callback route for google-auth

---
+++ src/components/AuthCallback.js
@@ -0,0 +1,23 @@
+import { useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import Cookies from 'js-cookie';
+import jwtDecode from 'jwt-decode';
+import { setUser } from './components/notes/notesSlice.js';
+
+// This component serves as an entry point for
+// a user at the end of the google-auth process.
+// It gives us a convenient way to set user state.
+export const AuthCallback = () => {
+
+const dispatch = useDispatch();
+const history = useHistory();
+
+const token = Cookies.get('token');
+const decodedToken = jwtDecode(token);
+const userId = decodedToken.id;
+
+dispatch(setUser({ userId, loggedIn: true}))
+history.push('/notes');
+
+return null;
+}
