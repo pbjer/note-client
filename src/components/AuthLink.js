0befc2a73e9c31200cb98706d1d6b01477f436d1
pbjer <prenticebjerk@gmail.com>
2020-06-16 11:51:23 -0700

    create Authlink to handle login/logout functionality

---
+++ src/components/AuthLink.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import Cookies from 'js-cookie';
+import { setUser } from './features/notes/notesSlice';
+
+export const AuthLink = (props) => {
+  const dispatch = useDispatch();
+  const { loggedIn } = props;
+
+  const handleLogout = () => {
+    Cookies.remove('token');
+    dispatch(setUser({
+      loggedIn: false,
+      userId: ''
+    }));
+  }
+
+  if (!loggedIn) {
+    return (
+      <a
+        href="http://localhost:3030/auth/google"
+        className="nav-link auth-link">
+        Sign in
+      </a>
+    )
+  } else {
+    return (
+      <Link
+        to="/"
+        className="nav-link auth-link"
+        onClick={() => handleLogout()}>
+        Log out
+      </Link>
+    )
+  }
+}
