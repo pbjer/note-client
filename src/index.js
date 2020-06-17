4a839f175cda881fb222c54c34bbb92c6fe2bd41
pbjer <prenticebjerk@gmail.com>
2020-06-17 13:44:04 -0700

    use axios interceptors to help handle server errors - 401, 403, 404, 422

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 import store from './app/store';
+import { setRequestError } from './components/notes/noteSlice';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import axios from './lib/axios';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,6 +15,34 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// https://stackoverflow.com/a/52947618
+const { dispatch } = store;
+
+axios.interceptors.response.use((response) => {
+  return response;
+}, (error) => { return handleErrorResponse(error); });
+
+const handleErrorResponse = (e) => {
+  const { status } = e.response;
+  switch(status) {
+    case 401: window.location.replace(
+      `${process.env.REACT_APP_API_ROOT}/auth/google`
+      );
+      break
+    case 403: dispatch(setRequestError(403))
+      break
+    case 404: window.location.replace(
+      `${process.env.REACT_APP_CLIENT_ROOT}/404`
+      );
+      break
+    case 422: dispatch(setRequestError(403));
+      break
+    default: dispatch(setRequestError(e.response.status));
+  }
+  return Promise.reject(e);
+}
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
